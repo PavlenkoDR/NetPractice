9cbd24473da52e2934aede7ee2161b1c2dc2047a
PavlenkoDR <pavlenkocraft@gmail.com>
2019-06-13 13:55:22 +1000

    refactoring

--- MailSlot/Client/backup/mailslotclientunit1.pas
+++ MailSlot/Client/backup/mailslotclientunit1.pas
@@ -5,7 +5,7 @@ unit MailSlotClientUnit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls,Windows;
 
 type
 
@@ -38,28 +38,35 @@ begin
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
-VAR
-Mailslot:THANDLE;
-BytesWritten:DWORD;
-ServerName:Array[1..256] of char;
-i:Integer;
+var
+    Mailslot:THANDLE;
+    BytesWritten:DWORD;
+    ServerName:Array[1..256] of char;
+    i:Integer;
 begin
-For i:=1 to 256 do ServerName[i]:=#0;
-StrPCopy(@ServerName,'\\'+Edit1.Text+'\Mailslot\Myslot');
-Mailslot:= CreateFile(@ServerName,
-GENERIC_WRITE, FILE_SHARE_READ,
-Nil, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL,0);
-if Mailslot= INVALID_HANDLE_VALUE then
-begin
-ShowMessageFmt('Ошибка  %d при создании файла', [GetLastError]);
-exit;
-end;
-if (WriteFile(Mailslot, 'This is a test', 14, BytesWritten, nil) = False) then
-begin
-ShowMessageFmt('Ошибка %d при записи в файл', [GetLastError]);
-exit; end;
-ShowMessageFmt('Передано %d байт', [BytesWritten]);
-CloseHandle(Mailslot);
+    for i:=1 to 256 do
+    begin
+         ServerName[i]:=#0;
+    end;
+    StrPCopy(@ServerName,'\\'+Edit1.Text+'\Mailslot\Myslot');
+              Mailslot:= CreateFile(@ServerName,
+              GENERIC_WRITE, FILE_SHARE_READ,
+              Nil,
+              OPEN_EXISTING,
+              FILE_ATTRIBUTE_NORMAL,
+              0);
+    if Mailslot= INVALID_HANDLE_VALUE then
+    begin
+        ShowMessageFmt('Ошибка  %d при создании файла', [GetLastError]);
+        exit;
+    end;
+    if (not WriteFile(Mailslot, 'This is a test', 14, BytesWritten, nil)) then
+    begin
+    ShowMessageFmt('Ошибка %d при записи в файл', [GetLastError]);
+    exit;
+    end;
+    ShowMessageFmt('Передано %d байт', [BytesWritten]);
+    CloseHandle(Mailslot);
 end;
 
 end.
--- MailSlot/Server/mailslotserverunit1.pas
+++ MailSlot/Server/mailslotserverunit1.pas
@@ -31,45 +31,43 @@ implementation
 { TForm1 }
 
 procedure TForm1.Button1Click(Sender: TObject);
-VAR
-   Mailslot:THANDLE;
-   buffer:Array[1..80] of char;//Объявляется одномерный массив символьных //данных  buffer размерности 80
-   NumberOfBytesRead:DWORD;
-   ret:Boolean;
+var
+    Mailslot:THANDLE;
+    buffer:Array[1..80] of char;//Объявляется одномерный массив символьных //данных  buffer размерности 80
+    NumberOfBytesRead:DWORD;
+    ret:Boolean;
 begin
-// Создание mailslot
-   Mailslot:=CreateMailslot('\\.\Mailslot\Myslot',         // Адрес
-                            0,                             // макс размер сообщения. 0 - безразмерно
-                            MAILSLOT_WAIT_FOREVER,         // время ожидания чтения
-                            nil);                          // аттрибуты безопасности
+    // Создание mailslot
+    Mailslot:=CreateMailslot('\\.\Mailslot\Myslot',          // Адрес
+                              0,                             // макс размер сообщения. 0 - безразмерно
+                              MAILSLOT_WAIT_FOREVER,         // время ожидания чтения
+                              nil);                          // аттрибуты безопасности
 
-   if  MailSlot= INVALID_HANDLE_VALUE  then
-       begin
-            ShowMessageFmt('Ошибка создания mailslot %d ', [GetLastError]);
-            exit;
-       end
-   else
-       begin
-           Label1.Caption:='Состояние сервера: Сервер запущен.';
-           Application.ProcessMessages;
-       end;
-// Чтение данных mailslot (блокирующий вызов)
-ret:=ReadFile(Mailslot,                   // читаем из постового ящика
-              buffer,                     // буффер куда читаем
-              256,                        // сколько данных может быть считано из буфера
-              NumberOfBytesRead,          // сколько байт реально считано по завершении функции
-              Nil);                       // параметр overlapped. nil - блокирующий режим.
-If ret then
-   begin
-     buffer[NumberOfBytesRead]:=#0;
-     ShowMessage(buffer);
-   end
-else
-   begin
-     ShowMessageFmt('Ошибка %d при чтении из mailslot', [GetLastError]);
-     exit;
-   end;
-   Label1.Caption:='Состояние сервера: Не запущен.';
+    if  MailSlot= INVALID_HANDLE_VALUE then
+    begin
+        ShowMessageFmt('Ошибка создания mailslot %d ', [GetLastError]);
+        exit;
+    end else begin
+        Label1.Caption:='Состояние сервера: Сервер запущен.';
+        Application.ProcessMessages;
+    end;
+
+    // Чтение данных mailslot (блокирующий вызов)
+    ret:=ReadFile(Mailslot,                   // читаем из постового ящика
+                  buffer,                     // буффер куда читаем
+                  256,                        // сколько данных может быть считано из буфера
+                  NumberOfBytesRead,          // сколько байт реально считано по завершении функции
+                  Nil);                       // параметр overlapped. nil - блокирующий режим.
+    If ret then
+    begin
+        buffer[NumberOfBytesRead]:=#0;
+        ShowMessage(buffer);
+    end else begin
+        ShowMessageFmt('Ошибка %d при чтении из mailslot', [GetLastError]);
+        exit;
+    end;
+
+    Label1.Caption:='Состояние сервера: Не запущен.';
 end;
 
 
