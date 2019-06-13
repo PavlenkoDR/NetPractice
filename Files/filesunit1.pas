2f9b410b1e1e5a10bff8b38648657b2674892586
PavlenkoDR <pavlenkocraft@gmail.com>
2019-06-13 13:48:03 +1000

    refactoring

--- Files/filesunit1.pas
+++ Files/filesunit1.pas
@@ -54,25 +54,31 @@ procedure TForm1.Button2Click(Sender: TObject);
     ReadString:string;
     UNCFileName:string;
 begin
-      UNCFileName:=Edit1.Text;
+    UNCFileName:=Edit1.Text;
+    FileHandle:= CreateFile(PChar(UNCFileName),
+                            GENERIC_WRITE or GENERIC_READ,
+                            FILE_SHARE_READ or FILE_SHARE_WRITE, Nil,
+                            OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
 
+    If FileHandle=INVALID_HANDLE_VALUE then
+    begin
+       ShowMessage(SysErrorMessage(GetLastError))
+    end else begin
+        ZeroMemory(@buffer, sizeof(buffer));
+    end;
 
-       FileHandle:= CreateFile(PChar(UNCFileName),
-                          GENERIC_WRITE or GENERIC_READ,
-                          FILE_SHARE_READ or FILE_SHARE_WRITE, Nil,
-                          OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
-
-       If FileHandle=INVALID_HANDLE_VALUE then ShowMessage(SysErrorMessage(GetLastError))
-       else
-        ZeroMemory(@buffer, sizeof(buffer)) ;
-        if(ReadFile(FileHandle, buffer, sizeOf(Buffer), BytesRead, Nil) = False) then ShowMessage(SysErrorMessage(GetLastError))
-           else
-           begin
-            ReadString:=StrPas(@buffer);
-            ShowMessage(readString);
-            Memo1.Lines.Text:=ReadString;
-                 if (CloseHandle(FileHandle) = False) then ShowMessage(SysErrorMessage(GetLastError))
-           end;
+    if(ReadFile(FileHandle, buffer, sizeOf(Buffer), BytesRead, Nil) = False) then
+    begin
+        ShowMessage(SysErrorMessage(GetLastError))
+    end else begin
+        ReadString:=StrPas(@buffer);
+        ShowMessage(readString);
+        Memo1.Lines.Text:=ReadString;
+        if (CloseHandle(FileHandle) = False) then
+        begin
+           ShowMessage(SysErrorMessage(GetLastError))
+        end;
+    end;
 
 end;
 
@@ -85,57 +91,47 @@ procedure TForm1.Button3Click(Sender: TObject);
     stringToWrite:string;
     UNCFileName:string;
 begin
-// Получить дескриптор \\.\C:\sample.txt
+    // Получить дескриптор \\.\C:\sample.txt
+    UNCFileName:=Edit1.Text;
+    FileHandle:= CreateFile(PChar(UNCFileName),
+                            GENERIC_WRITE or GENERIC_READ,
+                            FILE_SHARE_READ or FILE_SHARE_WRITE, Nil,
+                            CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
 
-       UNCFileName:=Edit1.Text;
+    If FileHandle=INVALID_HANDLE_VALUE then
+    begin
+       ShowMessage(SysErrorMessage(GetLastError))
+    end else begin
+        stringToWrite:=Memo1.Lines.Text;
+        ZeroMemory(@buffer, sizeof(buffer)) ;
+        StrPCopy(@buffer, PChar(stringToWrite));
+        BytesToWrite:=length(stringToWrite);
 
-
-       FileHandle:= CreateFile(PChar(UNCFileName),
-                          GENERIC_WRITE or GENERIC_READ,
-                          FILE_SHARE_READ or FILE_SHARE_WRITE, Nil,
-                          CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
-
-     If FileHandle=INVALID_HANDLE_VALUE  then
-          ShowMessage(SysErrorMessage(GetLastError))
-     else
-      begin
-
-       stringToWrite:=Memo1.Lines.Text;
-
-       ZeroMemory(@buffer, sizeof(buffer)) ;
-       StrPCopy(@buffer, PChar(stringToWrite));
-       BytesToWrite:=length(stringToWrite);
-
-
-       if (WriteFile(FileHandle, buffer, BytesToWrite, BytesWritten, Nil)) then
-         begin
-          ShowMessage('Файл записан!');
-          if not (CloseHandle(FileHandle))then             ShowMessage(SysErrorMessage(GetLastError));
-         end
-       else           ShowMessage(SysErrorMessage(GetLastError));
-     end;
+        if (WriteFile(FileHandle, buffer, BytesToWrite, BytesWritten, Nil)) then
+        begin
+             ShowMessage('Файл записан!');
+             if not (CloseHandle(FileHandle)) then
+             begin
+                ShowMessage(SysErrorMessage(GetLastError));
+             end;
+        end else begin
+            ShowMessage(SysErrorMessage(GetLastError));
+        end;
+    end;
 
 end;
 
 procedure TForm1.Button4Click(Sender: TObject);
 begin
-
- If MessageDlg('Вы действительно хотите удалить файл ?',mtWarning, mbYesNo, 0)=mrYes Then
-   begin
-    if DeleteFile(PChar(Edit1.Text)) then ShowMessage('Файл удален!')
-    else ShowMessage(SysErrorMessage(GetLastError));
-   end;
+    If MessageDlg('Вы действительно хотите удалить файл ?',mtWarning, mbYesNo, 0)=mrYes then
+    begin
+        if DeleteFile(PChar(Edit1.Text)) then
+        begin
+           ShowMessage('Файл удален!')
+        end else begin
+            ShowMessage(SysErrorMessage(GetLastError));
+        end;
+    end;
 end;
-
-
-
-
-
-
-
-
-
-
-
 end.
 
