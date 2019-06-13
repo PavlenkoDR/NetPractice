3ce93d72ac70d13b3cc3679afb1454788a4db28d
PavlenkoDR <pavlenkocraft@gmail.com>
2019-06-13 14:24:24 +1000

    restyled

--- TCP Socket/Client/tcpsocketclientunit1.pas
+++ TCP Socket/Client/tcpsocketclientunit1.pas
@@ -37,60 +37,61 @@ begin
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
-VAR
+var
    s: TSocket; {Сокет}
    recipient, sender_socket: TSockAddr; {адреса сокета получателя и сокета отправителя}
    ret: Integer; {результат работы функций}
    wsd: WSADATA; {Структура WSADATA, требуется для инициали-зации интерфейса сокетов}
-   bufr: Array[0..90] of char; {буфер для передачи данных на сервер}
-   msgstring: Array[0..90] of char {буфер для вывода сообщения на экран};
-   bufs: Array [1..80] of char; {буфер для получения ответа от сервера}
+   bufr: array[0..90] of char; {буфер для передачи данных на сервер}
+   msgstring: array[0..90] of char {буфер для вывода сообщения на экран};
+   bufs: array [1..80] of char; {буфер для получения ответа от сервера}
    size_socket: Integer; {размер структуры TSockAddr}
 begin
-     // Инициализация интерфейса сокетов
-     If WSAStartup(MAKEWORD(2,2),wsd)<>0   then ShowMessageFmt('Ошибка %d при инициализации интерфейса сокетов', [WSAGetLastError])
-     else
-         begin
-              // Создание сокета клиента
-              s :=socket(AF_INET, SOCK_STREAM,0);
-              if s=INVALID_SOCKET then ShowMessageFmt('Ошибка %d при создании сокета', [WSAGetLastError])
-              else
-                  begin
-                  // Формирование адреса сервера (получателя сообщения)
-                     ZeroMemory(@recipient, sizeof(recipient));
-                     recipient.sin_family :=AF_INET;
-                     recipient.sin_port:=htons(2000);
-                     recipient.sin_addr.S_addr := inet_addr('127.0.0.1');
-                     // Установление соединения
-                     ret:=connect(s, recipient, sizeof(recipient));
-                     If ret=SOCKET_ERROR then ShowMessageFmt('Ошибка %d при установлении соединения', [WSAGetLastError])
-                     else
-                         begin
-                              //Передаваемое сообщение
-                              strcopy(@bufs, 'Это мой тест');
-                              // Передача сообщения серверу
-                              ret := send(s, bufs, sizeof(bufs), 0);
-                              If ret=SOCKET_ERROR then ShowMessageFmt('Ошибка %d при передаче данных', [WSAGetLastError]);
-                              ZeroMemory(@bufr, sizeof(bufr));
-                              Size_socket := Sizeof(sender_socket);
-                              // Получение ответа от сервера
-                              ret:=recv(s, bufr, sizeof(bufr), 0);
-                                 If ret=SOCKET_ERROR then ShowMessageFmt('Ошибка %d при получении ответа', [WSAGetLastError])
-                                 else
-                                     begin
-                                          //Формирование сообщения msgstring дня вывода на экран
-                                          StrCopy(@msgstring, 'Получен ответ:');
-                                          StrCat(@msgstring, @bufr);
-                                          StrCat(@msgstring,' от');
-                                          StrCat(@msgstring, inet_ntoa(recipient.sin_addr));
-                                          ShowMessage(msgstring);
-                                     end;
-                         end;
-// закрытие сокета
-CloseSocket(s);
-end;
-WSACleanup;
-end;
+    // Инициализация интерфейса сокетов
+    If WSAStartup(MAKEWORD(2,2),wsd)<>0   then ShowMessageFmt('Ошибка %d при инициализации интерфейса сокетов', [WSAGetLastError])
+    else begin
+        // Создание сокета клиента
+        s :=socket(AF_INET, SOCK_STREAM,0);
+        if s=INVALID_SOCKET then
+            ShowMessageFmt('Ошибка %d при создании сокета', [WSAGetLastError])
+        else begin
+            // Формирование адреса сервера (получателя сообщения)
+            ZeroMemory(@recipient, sizeof(recipient));
+            recipient.sin_family :=AF_INET;
+            recipient.sin_port:=htons(2000);
+            recipient.sin_addr.S_addr := inet_addr('127.0.0.1');
+
+            // Установление соединения
+            ret:=connect(s, recipient, sizeof(recipient));
+            If ret=SOCKET_ERROR then
+                ShowMessageFmt('Ошибка %d при установлении соединения', [WSAGetLastError])
+            else begin
+                //Передаваемое сообщение
+                strcopy(@bufs, 'Это мой тест');
+
+                // Передача сообщения серверу
+                ret := send(s, bufs, sizeof(bufs), 0);
+                If ret=SOCKET_ERROR then ShowMessageFmt('Ошибка %d при передаче данных', [WSAGetLastError]);
+                ZeroMemory(@bufr, sizeof(bufr));
+                Size_socket := Sizeof(sender_socket);
+
+                // Получение ответа от сервера
+                ret:=recv(s, bufr, sizeof(bufr), 0);
+                if ret=SOCKET_ERROR then ShowMessageFmt('Ошибка %d при получении ответа', [WSAGetLastError])
+                else begin
+                    //Формирование сообщения msgstring дня вывода на экран
+                    StrCopy(@msgstring, 'Получен ответ:');
+                    StrCat(@msgstring, @bufr);
+                    StrCat(@msgstring,' от');
+                    StrCat(@msgstring, inet_ntoa(recipient.sin_addr));
+                    ShowMessage(msgstring);
+                end;
+            end;
+            // закрытие сокета
+            CloseSocket(s);
+        end;
+        WSACleanup;
+    end;
 end;
 
 
