01ab82481b113072b9de60c6d00808a66aec7b5e
PavlenkoDR <pavlenkocraft@gmail.com>
2019-06-13 14:13:57 +1000

    restyling

--- ScanNetOneThread/backup/scannetunit1.pas
+++ ScanNetOneThread/backup/scannetunit1.pas
@@ -35,87 +35,57 @@ implementation
 
 {$R *.lfm}
 
-procedure scan;
-   VAR
-    s: TSocket; {Сокет}
-    recipient: TSockAddr; {адрес сокета получателя}
-    ret: Integer; {результат работы функций}
-    j: Integer;
-    addr: Array[1..20] of char;
-begin
-     j := i;
-     StrPCopy(@addr, Form1.Edit1.text);
-     // Создание сокета клиента
-     s := socket(AF_INET,SOCK_STREAM, 0);
-     If s=INVALID_SOCKET then ShowMessageFmt('Ошибка %d при создании сокета', [WSAGetLastError])
-     else
-         begin
-              // Формирование адреса сервера (получателя сообщения)
-              ZeroMemory(@recipient,sizeof(recipient));
-              recipient.sin_family := AF_INET;
-              recipient.sin_port := htons(i);
-              recipient.sin_addr.S_addr := inet_addr(@addr);
-              // Установление соединения
-              ret := connect(s, recipient, sizeof(recipient));
-              If ret=SOCKET_ERROR then Form1.Memo1.Lines.Add('Порт '+IntToStr(j)+' не отвечает')
-              else
-                  Form1.Memo1.Lines.Add('Порт '+IntToStr(j)+' соединение установ-лено');
-              // закрытие сокета
-              CloseSocket(s);
-         end;
-end;
+
 
 { TForm1 }
 
 procedure TForm1.Button1Click(Sender: TObject);
-   VAR
-      hThread: Array [1..100] of THandle;
-      pFunc: Array [1..100] of pointer;
-      ThreadID: Array [1..100] of CARDINAL;
-      wsd: WSADATA; {Структура WSADATA, требуется для инициа-лизации интерфейса сокетов}
-      k, n,ii: Integer;
+var
+    hThread: array [1..100] of THandle;
+    pFunc: array [1..100] of pointer;
+    ThreadID: array [1..100] of CARDINAL;
+    wsd: WSADATA; {Структура WSADATA, требуется для инициа-лизации интерфейса сокетов}
+    k, n,ii: Integer;
 
     s: TSocket; {Сокет}
     recipient: TSockAddr; {адрес сокета получателя}
     ret: Integer; {результат работы функций}
     j: Integer;
-    addr: Array[1..20] of char;
+    addr: array[1..20] of char;
 begin
-   // Инициализация интерфейса сокетов
-   If WSAStartup(MAKEWORD(2,2), wsd)<>0 then ShowMessageFmt('Ошибка %d при инициализации интерфей-са сокетов', [WSAGetLastError])
-   else
-       begin
-           ii:=1;
-            k := StrToInt(Edit2.Text); n := StrToInt(Edit3.Text);
-             For i := k to n do
-                begin
-                   StrPCopy(@addr, Form1.Edit1.text);
-                       // Создание сокета клиента
-                       s := socket(AF_INET,SOCK_STREAM, 0);
-                       If s=INVALID_SOCKET then ShowMessageFmt('Ошибка %d при создании сокета', [WSAGetLastError])
-                       else
-                           begin
-                                // Формирование адреса сервера (получателя сообщения)
-                                ZeroMemory(@recipient,sizeof(recipient));
-                                recipient.sin_family := AF_INET;
-                                recipient.sin_port := htons(i);
-                                recipient.sin_addr.S_addr := inet_addr(@addr);
-                                // Установление соединения
-                                ret := connect(s, recipient, sizeof(recipient));
-                                If ret=SOCKET_ERROR then Form1.Memo1.Lines.Add('Порт '+IntToStr(j)+' не отвечает')
-                                else
-                                    Form1.Memo1.Lines.Add('Порт '+IntToStr(j)+' соединение установ-лено');
-                                // закрытие сокета
-                                CloseSocket(s);
-                           end;
-                end;
-       //WSACleanup;
-       end;
+    // Инициализация интерфейса сокетов
+    If WSAStartup(MAKEWORD(2,2), wsd)<>0 then
+        ShowMessageFmt('Ошибка %d при инициализации интерфей-са сокетов', [WSAGetLastError])
+    else begin
+        ii:=1;
+        k := StrToInt(Edit2.Text); n := StrToInt(Edit3.Text);
+        for i := k to n do
+        begin
+            StrPCopy(@addr, Form1.Edit1.text);
+
+            // Создание сокета клиента
+            s := socket(AF_INET,SOCK_STREAM, 0);
+            If s=INVALID_SOCKET then
+                ShowMessageFmt('Ошибка %d при создании сокета', [WSAGetLastError])
+            else begin
+                // Формирование адреса сервера (получателя сообщения)
+                ZeroMemory(@recipient,sizeof(recipient));
+                recipient.sin_family := AF_INET;
+                recipient.sin_port := htons(i);
+                recipient.sin_addr.S_addr := inet_addr(@addr);
+
+                // Установление соединения
+                ret := connect(s, recipient, sizeof(recipient));
+                If ret=SOCKET_ERROR then Form1.Memo1.Lines.Add('Порт '+IntToStr(i)+' не отвечает')
+                else
+                Form1.Memo1.Lines.Add('Порт '+IntToStr(i)+' соединение установ-лено');
+
+                // закрытие сокета
+                CloseSocket(s);
+            end;
+        end;
+        //WSACleanup;
+    end;
 end;
-
-
-
-
-
 end.
 
